049870db2409738002665f6072036457c1be7262
99mini <itman390@gmail.com>
2022-11-30 03:54:18 -1000

    add serail.ino

---
+++ Serial/Serial.ino
@@ -0,0 +1,59 @@
+String Speed;
+char DataToRead[2];
+int speed= 0;
+
+int a1 = 4;
+int a2 = 5;
+int b1 = 6;
+int b2 = 7;
+
+void setup(){
+	Serial.begin(9600);
+	pinMode(a1,OUTPUT);
+	pinMode(a2,OUTPUT);
+	pinMode(b1,OUTPUT);
+	pinMode(b2,OUTPUT);
+}
+
+void loop(){
+	DataToRead[1] = '\n';
+	
+	Serial.readBytesUntil(char(13),DataToRead, 2);
+	char direction = DataToRead[0];
+	
+	int i=1;
+	while(DataToRead[i] != '\n' && i < 2) i++;
+	
+	switch(direction){
+	case 'G':
+		analogWrite(a1,0);
+		analogWrite(a2,speed);
+		analogWrite(b1,speed);
+		analogWrite(b2,0);
+		delay(3000);
+		break;
+	case 'B':
+		analogWrite(a1,speed);
+		analogWrite(a2,0);
+		analogWrite(b1,0);
+		analogWrite(b2,speed);
+		delay(3000);
+		break;
+	case 'L':
+		analogWrite(a1,0);
+		analogWrite(a2,speed);
+		analogWrite(b1,0);
+		analogWrite(b2,0);
+		delay(3000);
+		break;
+	case 'R':
+		analogWrite(a1,0);
+		analogWrite(a2,0);
+		analogWrite(b1,speed);
+		analogWrite(b2,0);
+		delay(3000);
+		break;
+	default:
+		return;
+    }
+}
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import serial
 import cv2
 import numpy as np
-print(np.__version__)
-print(cv2.__version__)
 
 base_path = '/dev/serial/by-id/'
 by_id = 'usb-Arduino__www.arduino.cc__0043_758303334383510102E1-if00'
 ser = serial.Serial(base_path+by_id, 9600)
 
-print('test')
+print('start')
 
-direction = 'r'
+direction = 'G'
 
 
 cmd = ("%c\n" %direction).encode('ascii')
 print("My cmd is %s" % cmd)
 ser.write(cmd)
+
 #   For debugging, read cmd from arduino    
-read_serial=ser.readline()
-print (read_serial)
+#read_serial=ser.readline()
+#print(read_serial)
 
